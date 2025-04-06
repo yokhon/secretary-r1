6d1f23f880ad6cf103b02e0dc484cfa3d40d98f3
xhan <xhanab@connect.ust.hk>
2025-04-06 15:31:11 +0800

    Add customized QA reward manager.

---
+++ verl/workers/reward_manager/qa.py
@@ -0,0 +1,82 @@
+from verl import DataProto
+from verl.utils.reward_score import qa_em
+
+import torch
+import re
+
+
+def _select_rm_score_fn(data_source):
+    if data_source in ['nq', 'triviaqa', 'popqa', 'hotpotqa', '2wikimultihopqa', 'musique', 'bamboogle']:
+        return qa_em.compute_score_em
+    else:
+        raise NotImplementedError
+
+
+class QARewardManager():
+    """The reward manager.
+    """
+
+    def __init__(self, tokenizer, num_examine, format_score=0.) -> None:
+        self.tokenizer = tokenizer
+        self.num_examine = num_examine  # the number of batches of decoded responses to print to the console
+        self.format_score = format_score
+
+    def __call__(self, data: DataProto):
+        """We will expand this function gradually based on the available datasets"""
+
+        # If there is rm score, we directly return rm score. Otherwise, we compute via rm_score_fn
+        if 'rm_scores' in data.batch.keys():
+            return data.batch['rm_scores']
+
+        reward_tensor = torch.zeros_like(data.batch['responses'], dtype=torch.float32)
+
+        # all_scores = []
+
+        already_print_data_sources = {}
+
+        for i in range(len(data)):
+            data_item = data[i]  # DataProtoItem
+
+            prompt_ids = data_item.batch['prompts']
+
+            prompt_length = prompt_ids.shape[-1]
+
+            valid_prompt_length = data_item.batch['attention_mask'][:prompt_length].sum()
+            valid_prompt_ids = prompt_ids[-valid_prompt_length:]
+
+            response_ids = data_item.batch['responses']
+            valid_response_length = data_item.batch['attention_mask'][prompt_length:].sum()
+            valid_response_ids = response_ids[:valid_response_length]
+
+            # decode
+            sequences = torch.cat((valid_prompt_ids, valid_response_ids))
+            sequences_str = self.tokenizer.decode(sequences)
+
+            ground_truth = data_item.non_tensor_batch['reward_model']['ground_truth']
+
+            # select rm_score
+            data_source = data_item.non_tensor_batch['data_source']
+            compute_score_fn = _select_rm_score_fn(data_source)
+
+            score = compute_score_fn(solution_str=sequences_str, ground_truth=ground_truth,
+                                     format_score=self.format_score)
+
+            reward_tensor[i, valid_response_length - 1] = score
+            # all_scores.append(score)
+
+            if data_source not in already_print_data_sources:
+                already_print_data_sources[data_source] = 0
+
+            if already_print_data_sources[data_source] < self.num_examine:
+                already_print_data_sources[data_source] += 1
+                print(sequences_str)
+
+        # import numpy as np
+        # print(f"[DEBUG] all_scores: {all_scores}")
+        # print(f"[DEBUG] all_scores shape: {np.array(all_scores).shape}")
+        # print(f"[DEBUG] all_scores mean: {np.mean(all_scores)}")
+        # print(f"[DEBUG] all_scores max: {np.max(all_scores)}")
+        # print(f"[DEBUG] all_scores min: {np.min(all_scores)}")
+        # print(f"[DEBUG] all_scores std: {np.std(all_scores)}")
+
+        return reward_tensor
