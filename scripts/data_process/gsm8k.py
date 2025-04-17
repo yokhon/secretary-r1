92cd91d07bd1fafbacb9cfed72db4bd8bc5bc1f7
xhan <xhanab@connect.ust.hk>
2025-04-17 20:28:50 +0800

    Add swirl-v3 template for gsm8k.

--- scripts/data_process/gsm8k.py
+++ scripts/data_process/gsm8k.py
@@ -94,6 +94,13 @@ def make_prefix(dp, template_type):
         Once you have enough information, generate an answer enclosed by <answer>...</answer> tags. \
         Please either issue a calculation query or answer the question, but not both. \
         The question is: {question}\n"""
+    elif template_type == 'swirl-v3':
+        prefix = f"""Answer the given question. You must first conduct reasoning inside <think>...</think>. \
+        If you think it would help to use a calculator, please raise a mathematical query by <math_exp>...</math_exp> after <think>...</think>. \
+        Some questions may benefit from using a calculator multiple times in order to answer, so I will allow you to make up to 10 sequential queries before answering the question. \
+        Please do not repeat queries you have already issued, as this is a waste of time. \
+        When you have the final answer, you can output the answer inside <answer>...</answer>, without detailed illustrations. For example, <answer> 13 </answer>. \
+        The question is: {question}\n"""
     else:
         raise NotImplementedError
     return prefix
